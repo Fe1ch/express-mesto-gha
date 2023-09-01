69fe0ef1b389e66f08e8e74bb6fbe0f3892d998a
Михаил Мякинин <linkin-park_94@mail.ru>
2023-09-01 17:52:29 +0300

    version 1.4

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const usersRoutes = require('./users');
 const cardsRoutes = require('./cards');
 const signin = require('./signin');
 const signup = require('./signup');
-const notFound = require('./notFound');
+// const notFound = require('./notFound');
 const auth = require('../middlewares/auth');
 
 router.use('/signin', signin);
 router.use('/signup', signup);
 router.use('/users', auth, usersRoutes);
 router.use('/cards', auth, cardsRoutes);
-router.use('*', notFound);
+// router.use('*', notFound);
 
 module.exports = router;
